799f99e4ebc24fc212796e06a318704c7bf476f1
LD <laurent.deru@gmail.com>
2019-09-24 22:13:43 +0200

    Add GPL header to python files

--- cosmonium/cosmonium.py
+++ cosmonium/cosmonium.py
@@ -1,5 +1,22 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+#
+#This file is part of Cosmonium.
+#
+#Copyright (C) 2018-2019 Laurent Deru.
+#
+#Cosmonium is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Cosmonium is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Cosmonium.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 from __future__ import print_function
 from __future__ import absolute_import
 
