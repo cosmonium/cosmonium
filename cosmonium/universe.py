cosmonium/cosmonium

commit 666e5b0c43e336351b0a114133c845e3514ffdc5

Fix components visibility bug

cosmonium/universe.py
@@ -119,7 +119,11 @@ class Universe(StellarSystem):
 
     def first_update(self):
         for child in self.children:
-            child.update(0)
+            child.first_update(self.context.time.time_full)
+
+    def first_update_obs(self, observer):
+        for child in self.children:
+            child.first_update_obs(observer)
 
     def add_extra_to_list(self, *elems):
         for extra in elems:

cosmonium/annotations.py
@@ -141,7 +141,7 @@ class Orbit(VisibleObject):
         self.shader.update(self, self.appearance)
 
     def check_visibility(self, pixel_size):
-        if self.parent.shown and self.orbit:
+        if self.parent.parent.visible and self.parent.shown and self.orbit:
             distance_to_obs = self.parent.distance_to_obs
             if distance_to_obs > 0.0:
                 size = self.orbit.get_apparent_radius() / (distance_to_obs * pixel_size)

cosmonium/bodies.py
@@ -122,8 +122,8 @@ class StellarObject(LabelledObject):
         self.point_color = point_color
         self.abs_magnitude = 99.0
         #Flags
-        self.visible = True #TODO: Should be False at init
-        self.resolved = True #TODO: Should be False at init
+        self.visible = False
+        self.resolved = False
         self.in_view = False
         self.selected = False
         self.update_id = 0
@@ -393,6 +393,9 @@ class StellarObject(LabelledObject):
         else:
             return 0.0
 
+    def first_update(self, time):
+        self.update(time)
+
     def update(self, time):
         StellarObject.nb_update += 1
         self._orientation = self.rotation.get_rotation_at(time)
@@ -404,6 +407,9 @@ class StellarObject(LabelledObject):
             (self.vector_to_star, self.distance_to_star) = self.calc_local_distance_to(self.star.get_local_position())
         CompositeObject.update(self, time)
         self.update_frozen = not self.resolved and not (self.orbit.dynamic or self.rotation.dynamic)
+
+    def first_update_obs(self, observer):
+        self.update_obs(observer)
 
     def update_obs(self, observer):
         StellarObject.nb_obs += 1
@@ -478,9 +484,8 @@ class StellarObject(LabelledObject):
                     self.remove_components()
                 self.update_point(pointset)
         else:
-            if not self.resolved:
-                if self.init_components:
-                    self.remove_components()
+            if self.init_components:
+                self.remove_components()
         CompositeObject.check_and_update_instance(self, camera_pos, orientation, pointset)
 
     def remove_instance(self):

cosmonium/camera.py
@@ -170,6 +170,7 @@ class Camera(CameraBase):
         self.camera_frame = AbsoluteReferenceFrame()
         #Direction of sight of the camera
         self.camera_vector = LVector3d.forward()
+        self.update_camera()
 
     def set_camera_frame(self, frame):
         #Get position and rotation in the absolute reference frame

cosmonium/cosmonium.py
@@ -338,6 +338,7 @@ class Cosmonium(CosmoniumBase):
 
         self.time.set_current_date()
         self.universe.first_update()
+        self.universe.first_update_obs(self.observer)
         self.window_event(None)
         self.time_task(None)
 

cosmonium/systems.py
@@ -34,6 +34,7 @@ class StellarSystem(StellarObject):
         #Not used by StellarSystem, but used to detect SimpleSystem
         self.primary = None
         self.has_halo = False
+        self.was_visible = True
 
     def apply_func(self, func):
         StellarObject.apply_func(self, func)
@@ -170,12 +171,22 @@ class StellarSystem(StellarObject):
         for child in self.children:
             child.set_star(star)
 
+    def first_update(self, time):
+        StellarObject.update(self, time)
+        for child in self.children:
+            child.first_update(time)
+
     def update(self, time):
         StellarObject.update(self, time)
         #No need to update the children if not visible
         if not self.visible or not self.resolved: return
         for child in self.children:
             child.update(time)
+
+    def first_update_obs(self, observer):
+        StellarObject.update_obs(self, observer)
+        for child in self.children:
+            child.first_update_obs(observer)
 
     def update_obs(self, observer):
         StellarObject.update_obs(self, observer)

commit a1ca98bad24ad061b5e88108b32fbb8836176c46

Fix huge regression introduced by 666e5b0c43e336351b0a114133c845e3514ffdc5

cosmonium/universe.py
@@ -133,7 +133,10 @@ class Universe(StellarSystem):
     def update(self, time):
         CompositeObject.update(self, time)
         for leaf in self.to_update:
-            if not leaf.update_frozen:
+            if isinstance(leaf, StellarSystem):
+                #print("Update system", leaf.get_name())
+                leaf.update(time)
+            elif not leaf.update_frozen:
                 #print("Update", leaf.get_name())
                 leaf.update(time)
         for extra in self.to_update_extra: