a57bfa69f960e4e813514518bf1ebd143f55c691
LD <laurent.deru@gmail.com>
2020-01-05 14:49:15 +0100

    Add runtime configuration of mouse wheel direction

--- cosmonium/nav.py
+++ cosmonium/nav.py
@@ -58,18 +58,16 @@ class NavBase(object):
         pass
 
     def register_wheel_events(self, event_ctrl):
-        if settings.invert_wheel:
-            event_ctrl.accept("wheel_up", self.wheel_event, [1])
-            event_ctrl.accept("wheel_down", self.wheel_event, [-1])
-        else:
-            event_ctrl.accept("wheel_up", self.wheel_event, [-1])
-            event_ctrl.accept("wheel_down", self.wheel_event, [1])
+        event_ctrl.accept("wheel_up", self.wheel_event, [-1])
+        event_ctrl.accept("wheel_down", self.wheel_event, [1])
 
     def remove_wheel_events(self, event_ctrl):
         event_ctrl.ignore("wheel_up")
         event_ctrl.ignore("wheel_down")
 
     def wheel_event(self, direction):
+        if settings.invert_wheel:
+            direction = - direction
         self.wheel_event_time = globalClock.getRealTime()
         self.wheel_direction = direction
 
