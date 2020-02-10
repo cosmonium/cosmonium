cosmonium/cosmonium

commit 9cdfbf09e9ed8fff96d7f79724439226c96ddc68

Add runtime modification of elliptical orbits

cosmonium/annotations.py
@@ -108,7 +108,7 @@ class Orbit(VisibleObject):
             step = self.orbit.period * 10.0 / (self.nbOfPoints - 1)
         for i in range(self.nbOfPoints):
             time = epoch + step * i
-            pos = self.orbit.get_position_at(time) - delta
+            pos = self.orbit.get_frame_position_at(time) - delta
             self.vertexWriter.addData3f(*pos)
         self.lines = GeomLines(Geom.UHStatic)
         for i in range(self.nbOfPoints-1):
@@ -159,7 +159,7 @@ class Orbit(VisibleObject):
             self.place_instance_params(self.instance,
                                        self.body.parent.scene_position,
                                        self.body.parent.scene_scale_factor,
-                                       LQuaternion())
+                                       self.orbit.get_rotation_at(0))
             self.shader.update(self, self.appearance)
 
 class RotationAxis(VisibleObject):

cosmonium/bodies.py
@@ -38,6 +38,7 @@ from .astro import units
 from .shaders import BasicShader, FlatLightingModel
 from .bodyclass import bodyClasses
 from .catalogs import objectsDB
+from .parameters import ParametersGroup
 from . import settings
 from .settings import smallest_glare_mag
 from .utils import mag_to_scale
@@ -169,6 +170,19 @@ class StellarObject(LabelledObject):
             print("No class for", self.get_name())
             return
         self.set_shown(bodyClasses.get_show(self.body_class))
+
+    def get_user_parameters(self):
+        group = LabelledObject.get_user_parameters(self)
+        if isinstance(self.orbit, FixedOrbit) and self.system is not None:
+            orbit = self.system.orbit
+        else:
+            orbit = self.orbit
+        general_group = ParametersGroup('General', [orbit.get_user_parameters(),
+                                                    self.rotation.get_user_parameters()])
+        if group is None:
+            group = ParametersGroup(self.get_name(), [])
+        group.prepend_parameters(general_group)
+        return group
 
     def get_fullname(self, separator='/'):
         if hasattr(self, "primary") and self.primary is not None:

cosmonium/astro/orbits.py
@@ -28,6 +28,7 @@ from .kepler import kepler_pos
 from .astro import calc_orientation
 
 from math import pi, asin, atan2
+from cosmonium.parameters import ParametersGroup, UserParameter
 
 class Orbit(object):
     dynamic = False
@@ -36,6 +37,9 @@ class Orbit(object):
             frame = J2000EclipticReferenceFrame()
         self.frame = frame
         self.origin = LPoint3d()
+
+    def get_user_parameters(self):
+        return None
 
     def set_frame(self, frame):
         self.frame = frame
@@ -185,7 +189,9 @@ class EllipticalOrbit(Orbit):
         self.arg_of_periapsis = arg_of_periapsis * pi / 180
         self.mean_anomaly = mean_anomaly * pi / 180
         self.epoch = epoch
+        self.update_rotation()
 
+    def update_rotation(self):
         inclination_quat = LQuaterniond()
         inclination_quat.setFromAxisAngleRad(self.inclination, LVector3d.unitX())
         arg_of_periapsis_quat = LQuaterniond()
@@ -193,6 +199,35 @@ class EllipticalOrbit(Orbit):
         ascending_node_quat = LQuaterniond()
         ascending_node_quat.setFromAxisAngleRad(self.ascending_node, LVector3d.unitZ())
         self.rotation = arg_of_periapsis_quat * inclination_quat * ascending_node_quat
+
+    def set_inclination(self, inclination):
+        self.inclination = inclination * pi / 180
+        self.update_rotation()
+
+    def get_inclination(self):
+        return self.inclination / pi * 180
+
+    def set_arg_of_periapsis(self, arg_of_periapsis):
+        self.arg_of_periapsis = arg_of_periapsis * pi / 180
+        self.update_rotation()
+
+    def get_arg_of_periapsis(self):
+        return self.arg_of_periapsis / pi * 180
+
+    def set_ascending_node(self, ascending_node):
+        self.ascending_node = ascending_node * pi / 180
+        self.update_rotation()
+
+    def get_ascending_node(self):
+        return self.ascending_node / pi * 180
+
+    def get_user_parameters(self):
+        parameters = [UserParameter("Inclination", self.set_inclination, self.get_inclination, UserParameter.TYPE_FLOAT, value_range=[0, 360]),
+                      UserParameter("Argument of periapsis", self.set_arg_of_periapsis, self.get_arg_of_periapsis, UserParameter.TYPE_FLOAT, value_range=[0, 360]),
+                      UserParameter("Ascending node", self.set_ascending_node, self.get_ascending_node, UserParameter.TYPE_FLOAT, value_range=[0, 360]),
+                     ]
+        group = ParametersGroup('Orbit', parameters)
+        return group
 
     def is_periodic(self):
         return self.eccentricity < 1.0

cosmonium/astro/rotations.py
@@ -33,6 +33,9 @@ class Rotation(object):
     def __init__(self, frame):
         self.frame = frame
 
+    def get_user_parameters(self):
+        return None
+
     def set_frame(self, frame):
         self.frame = frame
 

cosmonium/parameters.py
@@ -33,6 +33,12 @@ class ParametersGroup(object):
         else:
             self.parameters += parameters
 
+    def prepend_parameters(self, *parameters):
+        if len(parameters) == 1 and isinstance(parameters[0], Iterable):
+            self.parameters = parameters[0] + self.parameters
+        else:
+            self.parameters = list(parameters) + self.parameters
+
     def is_group(self):
         return True
 

cosmonium/ui/editor.py
@@ -167,6 +167,7 @@ class ParamEditor():
 
     def add_parameters(self, frame, sizer, parameters):
         for param in parameters:
+            if param is None: continue
             if param.is_group():
                 borders = (self.font_size / 2, 0, self.font_size / 4.0, self.font_size / 4.0)
                 if param.name is not None:

commit fab217e7d2a508785227e57a602a2ddd38e5f6be

Fix regression introduced by 9cdfbf09e9ed8fff96d7f79724439226c96ddc68

cosmonium/annotations.py
@@ -21,7 +21,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from panda3d.core import LPoint3d, LQuaternion, LColor, LVector3, LVector3d
-from panda3d.core import GeomVertexFormat, GeomVertexData, GeomVertexWriter
+from panda3d.core import GeomVertexFormat, GeomVertexData, GeomVertexWriter, GeomVertexRewriter, InternalName
 from panda3d.core import Geom, GeomNode, GeomLines
 from panda3d.core import NodePath, AntialiasAttrib
 
@@ -108,7 +108,7 @@ class Orbit(VisibleObject):
             step = self.orbit.period * 10.0 / (self.nbOfPoints - 1)
         for i in range(self.nbOfPoints):
             time = epoch + step * i
-            pos = self.orbit.get_frame_position_at(time) - delta
+            pos = self.orbit.get_position_at(time) - delta
             self.vertexWriter.addData3f(*pos)
         self.lines = GeomLines(Geom.UHStatic)
         for i in range(self.nbOfPoints-1):
@@ -140,6 +140,24 @@ class Orbit(VisibleObject):
         self.shader.apply(self, self.appearance)
         self.shader.update(self, self.appearance)
 
+    def update_geom(self):
+        geom = self.node.modify_geom(0)
+        vdata = geom.modify_vertex_data()
+        vwriter = GeomVertexRewriter(vdata, InternalName.make('vertex'))
+        #TODO: refactor with above code !!!
+        delta = self.body.parent.get_local_position()
+        if self.orbit.is_periodic():
+            epoch = self.context.time.time_full - self.orbit.period
+            step = self.orbit.period / (self.nbOfPoints - 1)
+        else:
+            #TODO: Properly calculate orbit start and end time
+            epoch = self.orbit.get_time_of_perihelion() - self.orbit.period * 5.0
+            step = self.orbit.period * 10.0 / (self.nbOfPoints - 1)
+        for i in range(self.nbOfPoints):
+            time = epoch + step * i
+            pos = self.orbit.get_position_at(time) - delta
+            vwriter.setData3f(*pos)
+
     def check_visibility(self, pixel_size):
         if self.parent.parent.visible and self.parent.shown and self.orbit:
             distance_to_obs = self.parent.distance_to_obs
@@ -159,8 +177,11 @@ class Orbit(VisibleObject):
             self.place_instance_params(self.instance,
                                        self.body.parent.scene_position,
                                        self.body.parent.scene_scale_factor,
-                                       self.orbit.get_rotation_at(0))
+                                       LQuaternion())
             self.shader.update(self, self.appearance)
+
+    def update_user_parameters(self):
+        self.update_geom()
 
 class RotationAxis(VisibleObject):
     default_shown = False

cosmonium/bodies.py
@@ -185,6 +185,11 @@ class StellarObject(LabelledObject):
         group.prepend_parameters(general_group)
         return group
 
+    def update_user_parameters(self):
+        LabelledObject.update_user_parameters(self)
+        if isinstance(self.orbit, FixedOrbit) and self.system is not None and self.system.orbit_object is not None:
+            self.system.orbit_object.update_user_parameters()
+
     def get_fullname(self, separator='/'):
         if hasattr(self, "primary") and self.primary is not None:
             name = self.primary.get_friendly_name()