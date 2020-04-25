cosmonium/cosmonium

commit 6806fb8bd3d50a773426c575fe1db324706b9bd9

Fix wrong alpha channel in PBR render

cosmonium/pbr.py
@@ -179,7 +179,8 @@ vec3 calc_shade(PointMaterial material, PointVectors vectors)
         #TODO: Loop here over light sources
         code.append('PointVectors vectors = calc_point_vectors(normal, obs_dir, light_dir);')
         code.append("vec3 shade = calc_shade(material, vectors);")
-        code.append("total_diffuse_color = vectors.n_dot_l * light_color * vec4(shade, 1.0) * shadow;")
+        code.append("total_diffuse_color.rgb = vectors.n_dot_l * light_color.rgb * shade, 1.0 * shadow;")
+        code.append("total_diffuse_color.a = surface_color.a;")
 
         code.append("if (vectors.n_dot_l < 0.0) {")
         if self.appearance.has_night:

commit 0757ca2c01833f8f2388993c092c96b267ef3e5d

Fix PBR regression introduced by 6806fb8bd3d50a773426c575fe1db324706b9bd9

cosmonium/pbr.py
@@ -179,7 +179,7 @@ vec3 calc_shade(PointMaterial material, PointVectors vectors)
         #TODO: Loop here over light sources
         code.append('PointVectors vectors = calc_point_vectors(normal, obs_dir, light_dir);')
         code.append("vec3 shade = calc_shade(material, vectors);")
-        code.append("total_diffuse_color.rgb = vectors.n_dot_l * light_color.rgb * shade, 1.0 * shadow;")
+        code.append("total_diffuse_color.rgb = vectors.n_dot_l * light_color.rgb * shade * shadow;")
         code.append("total_diffuse_color.a = surface_color.a;")
         self.apply_emission(code, 'vectors.n_dot_l')
 