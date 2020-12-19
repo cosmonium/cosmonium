05ac29bada634c178286561384825a7f5d385604
LD <laurent.deru@gmail.com>
2020-12-19 21:52:32 +0100

    Add translation for the preference window

--- cosmonium/ui/preferences.py
+++ cosmonium/ui/preferences.py
@@ -46,97 +46,97 @@ class Preferences(ParamEditor):
         self.cosmonium.update_settings()
 
     def make_entries(self):
-        return ParametersGroup('Preferences',
-                               [ParametersGroup('General', self.make_general()),
-                                ParametersGroup('Orbits', self.make_orbits()),
-                                ParametersGroup('Labels', self.make_labels()),
-                                ParametersGroup('Render', self.make_render()),
-                                ParametersGroup('Advanced', self.make_advanced()),
-                                ParametersGroup('Debug', self.make_debug()),
+        return ParametersGroup(_('Preferences'),
+                               [ParametersGroup(_('General'), self.make_general()),
+                                ParametersGroup(_('Orbits'), self.make_orbits()),
+                                ParametersGroup(_('Labels'), self.make_labels()),
+                                ParametersGroup(_('Render'), self.make_render()),
+                                ParametersGroup(_('Advanced'), self.make_advanced()),
+                                ParametersGroup(_('Debug'), self.make_debug()),
                                 ])
 
 
     def make_general(self):
-        return [ParametersGroup('UI',
+        return [ParametersGroup(_('UI'),
                                 []),
-                ParametersGroup('Mouse',
-                                [SettingParameter('Invert mouse wheel', 'invert_wheel', SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Mouse'),
+                                [SettingParameter(_('Invert mouse wheel'), 'invert_wheel', SettingParameter.TYPE_BOOL),
                                  ]),
-                ParametersGroup('Keyboard',
-                                [SettingParameter('Damped navigation keys', 'damped_nav', SettingParameter.TYPE_BOOL),
-                                 SettingParameter('Invert Up/Down and Left/Right', 'celestia_nav', SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Keyboard'),
+                                [SettingParameter(_('Damped navigation keys'), 'damped_nav', SettingParameter.TYPE_BOOL),
+                                 SettingParameter(_('Invert Up/Down and Left/Right'), 'celestia_nav', SettingParameter.TYPE_BOOL),
                                  ])
                ]
 
     def make_orbits(self):
         orbits = [
-                ('Stars', 'star'),
-                ('Planets', 'planet'),
-                ('Dwarf planets','dwarfplanet'),
-                ('Moons', 'moon'),
-                ('Minor moons', 'minormoon'),
-                ('Lost moons', 'lostmoon'),
-                ('Comets', 'comet'),
-                ('Asteroids', 'asteroid'),
-                ('Interstellars', 'interstellar'),
-                ('Spacecrafts', 'spacecraft'),
+                (_('Stars'), 'star'),
+                (_('Planets'), 'planet'),
+                (_('Dwarf planets'),'dwarfplanet'),
+                (_('Moons'), 'moon'),
+                (_('Minor moons'), 'minormoon'),
+                (_('Lost moons'), 'lostmoon'),
+                (_('Comets'), 'comet'),
+                (_('Asteroids'), 'asteroid'),
+                (_('Interstellars'), 'interstellar'),
+                (_('Spacecrafts'), 'spacecraft'),
                ]
-        params = [ SettingParameter('All orbits', 'show_orbits', SettingParameter.TYPE_BOOL) ]
+        params = [ SettingParameter(_('All orbits'), 'show_orbits', SettingParameter.TYPE_BOOL) ]
         params += [ParametricFunctionParameter(label, param_name, bodyClasses.set_show_orbit, bodyClasses.get_show_orbit, SettingParameter.TYPE_BOOL)
                 for (label, param_name) in orbits]
         return params
 
     def make_labels(self):
         labels = [
-                  ('Galaxies', 'galaxy'),
-                  #('Globular', 'globular'),
-                  ('Stars', 'star'),
-                  ('Planets', 'planet'),
-                  ('Dwarf planets', 'dwarfplanet'),
-                  ('Moons', 'moon'),
-                  ('Minor moons', 'minormoon'),
-                  ('Lost moons', 'lostmoon'),
-                  ('Comets', 'comet'),
-                  ('Asteroids', 'asteroid'),
-                  ('Interstellars', 'interstellar'),
-                  ('Spacecrafts', 'spacecraft'),
-                  ('Constellations', 'constellation'),
-                  #('Locations', 'location'),
+                  (_('Galaxies'), 'galaxy'),
+                  #(_('Globular'), 'globular'),
+                  (_('Stars'), 'star'),
+                  (_('Planets'), 'planet'),
+                  (_('Dwarf planets'), 'dwarfplanet'),
+                  (_('Moons'), 'moon'),
+                  (_('Minor moons'), 'minormoon'),
+                  (_('Lost moons'), 'lostmoon'),
+                  (_('Comets'), 'comet'),
+                  (_('Asteroids'), 'asteroid'),
+                  (_('Interstellars'), 'interstellar'),
+                  (_('Spacecrafts'), 'spacecraft'),
+                  (_('Constellations'), 'constellation'),
+                  #(_('Locations'), 'location'),
                 ]
-        return [ParametersGroup('Labels',
+        return [ParametersGroup(_('Labels'),
                                 [ParametricFunctionParameter(label, param_name, bodyClasses.set_show_label, bodyClasses.get_show_label, SettingParameter.TYPE_BOOL)
                                  for (label, param_name) in labels]),
-                ParametersGroup('Fonts',
-                                [SettingParameter("Label size", 'label_size', UserParameter.TYPE_INT, [4, 32]),
-                                 SettingParameter("Constellation label size", 'constellations_label_size', UserParameter.TYPE_INT, [4, 32]),
+                ParametersGroup(_('Fonts'),
+                                [SettingParameter(_("Label size"), 'label_size', UserParameter.TYPE_INT, [4, 32]),
+                                 SettingParameter(_("Constellation label size"), 'constellations_label_size', UserParameter.TYPE_INT, [4, 32]),
                                  ]),
                 ]
 
     def make_render(self):
         return [
-                ParametersGroup('Objects',
-                                [ParametricFunctionParameter('Galaxies', 'galaxy',bodyClasses.set_show, bodyClasses.get_show, SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Objects'),
+                                [ParametricFunctionParameter(_('Galaxies'), 'galaxy',bodyClasses.set_show, bodyClasses.get_show, SettingParameter.TYPE_BOOL),
                                 ]),
                 #('shift-u', self.cosmonium.toggle_globulars)
                 #('^', self.cosmonium.toggle_nebulae)
-                ParametersGroup('Components',
-                                [SettingParameter('Atmospheres', 'show_atmospheres', SettingParameter.TYPE_BOOL),
-                                 SettingParameter('Clouds', 'show_clouds', SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Components'),
+                                [SettingParameter(_('Atmospheres'), 'show_atmospheres', SettingParameter.TYPE_BOOL),
+                                 SettingParameter(_('Clouds'), 'show_clouds', SettingParameter.TYPE_BOOL),
                                  ]),
                 #('control-e', self.toggle_shadows)
                 #('control-l', self.cosmonium.toggle_nightsides)
                 #('control-t', self.cosmonium.toggle_comet_tails)
-                ParametersGroup('Constellations',
-                                [SettingParameter('Boundaries', 'show_boundaries', SettingParameter.TYPE_BOOL),
-                                 SettingParameter('Asterisms', 'show_asterisms', SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Constellations'),
+                                [SettingParameter(_('Boundaries'), 'show_boundaries', SettingParameter.TYPE_BOOL),
+                                 SettingParameter(_('Asterisms'), 'show_asterisms', SettingParameter.TYPE_BOOL),
                                  ]),
-                ParametersGroup('Grids',
-                                [SettingParameter('Equatorial', 'show_equatorial_grid', SettingParameter.TYPE_BOOL),
-                                 SettingParameter('Ecliptic', 'show_ecliptic_grid', SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Grids'),
+                                [SettingParameter(_('Equatorial'), 'show_equatorial_grid', SettingParameter.TYPE_BOOL),
+                                 SettingParameter(_('Ecliptic'), 'show_ecliptic_grid', SettingParameter.TYPE_BOOL),
                                  ]),
-                ParametersGroup('Annotations',
-                                [SettingParameter('Rotation axis', 'show_rotation_axis', SettingParameter.TYPE_BOOL),
-                                 SettingParameter('Reference frame', 'show_reference_axis', SettingParameter.TYPE_BOOL),
+                ParametersGroup(_('Annotations'),
+                                [SettingParameter(_('Rotation axis'), 'show_rotation_axis', SettingParameter.TYPE_BOOL),
+                                 SettingParameter(_('Reference frame'), 'show_reference_axis', SettingParameter.TYPE_BOOL),
                                  ]),
                 ]
 
@@ -147,35 +147,35 @@ class Preferences(ParamEditor):
         settings.ui_scale = (scale, scale)
 
     def make_advanced(self):
-        return [ParametersGroup('UI',
-                                [UserParameter("UI Scale", self.set_ui_scale, self.get_ui_scale, UserParameter.TYPE_FLOAT, [0.5, 2]),
-                                 SettingParameter("Menu text size", 'menu_text_size', UserParameter.TYPE_INT, [4, 32]),
-                                 SettingParameter("UI text size", 'ui_font_size', UserParameter.TYPE_INT, [4, 32]),
+        return [ParametersGroup(_('UI'),
+                                [UserParameter(_("UI Scale"), self.set_ui_scale, self.get_ui_scale, UserParameter.TYPE_FLOAT, [0.5, 2]),
+                                 SettingParameter(_("Menu text size"), 'menu_text_size', UserParameter.TYPE_INT, [4, 32]),
+                                 SettingParameter(_("UI text size"), 'ui_font_size', UserParameter.TYPE_INT, [4, 32]),
                                 ]),
-                ParametersGroup('HUD',
-                                [SettingParameter("HUD text size", 'hud_text_size', UserParameter.TYPE_INT, [4, 32]),
-                                 SettingParameter("HUD info size", 'hud_info_text_size', UserParameter.TYPE_INT, [4, 32]),
-                                 SettingParameter("HUD color", 'hud_color', UserParameter.TYPE_VEC, [0, 1], nb_components=3),
+                ParametersGroup(_('HUD'),
+                                [SettingParameter(_("HUD text size"), 'hud_text_size', UserParameter.TYPE_INT, [4, 32]),
+                                 SettingParameter(_("HUD info size"), 'hud_info_text_size', UserParameter.TYPE_INT, [4, 32]),
+                                 SettingParameter(_("HUD color"), 'hud_color', UserParameter.TYPE_VEC, [0, 1], nb_components=3),
                                 ]),
-                ParametersGroup('Query',
-                                [SettingParameter("Query size", 'query_text_size', UserParameter.TYPE_INT, [4, 32]),
-                                 SettingParameter("Suggestion size", 'query_suggestion_text_size', UserParameter.TYPE_INT, [4, 32]),
+                ParametersGroup(_('Query'),
+                                [SettingParameter(_("Query size"), 'query_text_size', UserParameter.TYPE_INT, [4, 32]),
+                                 SettingParameter(_("Suggestion size"), 'query_suggestion_text_size', UserParameter.TYPE_INT, [4, 32]),
                                 ]),
-                ParametersGroup('Render',
-                                [SettingParameter("Cull far patches", 'cull_far_patches', UserParameter.TYPE_BOOL),
-                                 SettingParameter("Cull far patches threshold", 'cull_far_patches_threshold', UserParameter.TYPE_INT, [5, 25]),
-                                 SettingParameter("Shadow slope bias", 'shadows_slope_scale_bias', UserParameter.TYPE_BOOL),
-                                 SettingParameter("Shadow PCF", 'shadows_pcf_16', UserParameter.TYPE_BOOL),
-                                 SettingParameter("Shadow slope bias", 'shadows_slope_scale_bias', UserParameter.TYPE_BOOL),
+                ParametersGroup(_('Render'),
+                                [SettingParameter(_("Cull far patches"), 'cull_far_patches', UserParameter.TYPE_BOOL),
+                                 SettingParameter(_("Cull far patches threshold"), 'cull_far_patches_threshold', UserParameter.TYPE_INT, [5, 25]),
+                                 SettingParameter(_("Shadow slope bias"), 'shadows_slope_scale_bias', UserParameter.TYPE_BOOL),
+                                 SettingParameter(_("Shadow PCF"), 'shadows_pcf_16', UserParameter.TYPE_BOOL),
+                                 SettingParameter(_("Shadow slope bias"), 'shadows_slope_scale_bias', UserParameter.TYPE_BOOL),
                                 ]),
-                ParametersGroup('OpenGL',
-                                [SettingParameter("Sync video", 'sync_video', UserParameter.TYPE_BOOL),
-                                SettingParameter("Multisampling", 'multisamples', UserParameter.TYPE_INT, [0, 16])
+                ParametersGroup(_('OpenGL'),
+                                [SettingParameter(_("Sync video"), 'sync_video', UserParameter.TYPE_BOOL),
+                                SettingParameter(_("Multisampling"), 'multisamples', UserParameter.TYPE_INT, [0, 16])
                                 ]),
                 ]
     def make_debug(self):
-        return [ParametersGroup('Async',
-                                [SettingParameter("Sync data loading", 'sync_data_load', UserParameter.TYPE_BOOL),
-                                 SettingParameter("Sync texture loading", 'sync_texture_load', UserParameter.TYPE_BOOL),
+        return [ParametersGroup(_('Async'),
+                                [SettingParameter(_("Sync data loading"), 'sync_data_load', UserParameter.TYPE_BOOL),
+                                 SettingParameter(_("Sync texture loading"), 'sync_texture_load', UserParameter.TYPE_BOOL),
                                 ])
         ]
