2c47e8afb4135ed2882e067fc4393b94497b2367
LD <laurent.deru@gmail.com>
2020-12-23 21:53:40 +0100

    Remove obsolete workaround for numpy

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ config = {
             ],
             'package_data_dirs':
             {
-             'numpy': [('numpy.libs/*', '', {'PKG_DATA_MAKE_EXECUTABLE'})],
              'win32': [('pywin32_system32/*', '', {}),
                        ('win32/*.pyd', '', {}),
                        ('win32/lib/win32con.py', '', {})],
