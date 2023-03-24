b4e9cc7a22aa8e21ac17309a708472b7e11dc298
LD <laurent.deru@gmail.com>
2023-03-24 18:41:16 +0100

    procedural: Fix scale of star radiance

--- cosmonium/procedural/stars.py
+++ cosmonium/procedural/stars.py
@@ -1,7 +1,7 @@
 #
 #This file is part of Cosmonium.
 #
-#Copyright (C) 2018-2022 Laurent Deru.
+#Copyright (C) 2018-2023 Laurent Deru.
 #
 #Cosmonium is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@
 
 from math import pi
 
+from ..astro import units
 from ..components.elements.surfaces import EllipsoidFlatSurface
 from ..patchedshapes import SquaredDistanceSquareShape, SquaredDistanceSquarePatchFactory, VertexSizeLodControl
 from ..shaders.rendering import RenderingShader
@@ -47,7 +48,7 @@ class ProceduralStarSurfaceFactory(SurfaceFactory):
         shader = RenderingShader(lighting_model=FlatLightingModel())
         tex_generator = NoiseTextureGenerator(self.size, self.noise, self.target, alpha=False, srgb=False)
         if settings.use_pbr:
-            radiance = body.anchor.get_radiant_flux() / (4 * pi *pi * body.radius * body.radius)
+            radiance = body.anchor.get_radiant_flux() / (4 * pi * pi * body.radius * body.radius / units.m / units.m)
         else:
             radiance = 1.0
         surface = EllipsoidFlatSurface(radius=body.radius, oblateness=body.oblateness, scale=body.scale,
