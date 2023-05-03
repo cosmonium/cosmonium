e6bd2a120f67f4b18da27a7961b8df173f81c3e6
LD <laurent.deru@gmail.com>
2023-05-04 00:05:21 +0200

    shaders: Move fog after effect into after_effects

--- cosmonium/shaders/after_effects/fog.py
+++ cosmonium/shaders/after_effects/fog.py
@@ -1,7 +1,7 @@
 #
 #This file is part of Cosmonium.
 #
-#Copyright (C) 2018-2022 Laurent Deru.
+#Copyright (C) 2018-2023 Laurent Deru.
 #
 #Cosmonium is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 #
 
 
-from .component import ShaderComponent
+from ..component import ShaderComponent
 
 
 class Fog(ShaderComponent):
