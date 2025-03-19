690ec87b5f88cb6e22258e9a43b0068ca8c449f0
LD <laurent.deru@gmail.com>
2025-03-19 23:32:46 +0100

    shaders: Refactor CompositeShaderDataSource to use CompositeShaderComponent

--- cosmonium/shaders/data_source/base.py
+++ cosmonium/shaders/data_source/base.py
@@ -1,7 +1,7 @@
 #
 # This file is part of Cosmonium.
 #
-# Copyright (C) 2018-2024 Laurent Deru.
+# Copyright (C) 2018-2025 Laurent Deru.
 #
 # Cosmonium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 #
 
 
-from ..component import ShaderComponent
+from ..component import CompositeShaderComponent, ShaderComponent
 
 
 class ShaderDataSource(ShaderComponent):
@@ -31,104 +31,33 @@ class ShaderDataSource(ShaderComponent):
         return ''
 
 
-class CompositeShaderDataSource(ShaderDataSource):
+class CompositeShaderDataSource(CompositeShaderComponent):
     def __init__(self, sources=None):
-        ShaderDataSource.__init__(self)
-        if sources is None:
-            self.sources = []
-        elif isinstance(sources, list):
-            self.sources = sources
-        else:
-            self.sources = [sources]
-
-    def set_shader(self, shader):
-        self.shader = shader
-        for source in self.sources:
-            source.set_shader(shader)
-
-    def get_id(self):
-        str_id = ""
-        for source in self.sources:
-            src_id = source.get_id()
-            if src_id:
-                str_id += '-'
-                str_id += src_id
-        return str_id
+        CompositeShaderComponent.__init__(self)
+        if sources is not None:
+            if isinstance(sources, list):
+                for source in sources:
+                    self.add_component(source)
+            else:
+                self.add_component(sources)
 
     def add_source(self, source):
-        self.sources.append(source)
-        source.set_shader(self.shader)
+        self.add_component(source)
 
     def remove_source(self, source_id):
-        self.sources = list(filter(lambda source: not source.has_source_for(source_id), self.sources))
-
-    def create_shader_configuration(self, appearance):
-        for source in self.sources:
-            source.create_shader_configuration(appearance)
+        self.components = list(filter(lambda source: not source.has_source_for(source_id), self.components))
 
     def has_source_for(self, source_id):
-        for source in self.sources:
+        for source in self.components:
             if source.has_source_for(source_id):
                 return True
         return False
 
     def get_source_for(self, source_id, params=None, error=True):
-        for source in self.sources:
+        for source in self.components:
             value = source.get_source_for(source_id, params, False)
             if value != '':
                 return value
         if error:
             print("Unknown source '%s' requested" % source_id)
         return ''
-
-    def vertex_layout(self, code):
-        for source in self.sources:
-            source.vertex_layout(code)
-
-    def vertex_uniforms(self, code):
-        for source in self.sources:
-            source.vertex_uniforms(code)
-
-    def vertex_inputs(self, code):
-        for source in self.sources:
-            source.vertex_inputs(code)
-
-    def vertex_outputs(self, code):
-        for source in self.sources:
-            source.vertex_outputs(code)
-
-    def vertex_extra(self, code):
-        for source in self.sources:
-            source.vertex_extra(code)
-
-    def vertex_shader_decl(self, code):
-        for source in self.sources:
-            source.vertex_shader_decl(code)
-
-    def vertex_shader(self, code):
-        for source in self.sources:
-            source.vertex_shader(code)
-
-    def fragment_uniforms(self, code):
-        for source in self.sources:
-            source.fragment_uniforms(code)
-
-    def fragment_inputs(self, code):
-        for source in self.sources:
-            source.fragment_inputs(code)
-
-    def fragment_extra(self, code):
-        for source in self.sources:
-            source.fragment_extra(code)
-
-    def fragment_shader_decl(self, code):
-        for source in self.sources:
-            source.fragment_shader_decl(code)
-
-    def fragment_shader_distort_coord(self, code):
-        for source in self.sources:
-            source.fragment_shader_distort_coord(code)
-
-    def fragment_shader(self, code):
-        for source in self.sources:
-            source.fragment_shader(code)
